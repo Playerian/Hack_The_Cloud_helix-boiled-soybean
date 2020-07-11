48e6876d1bcb7ed627d64fa0661db7cbd03c6543
Glitch (hello-webpage) <none>
2020-07-11 20:00:35 +0000

    🎌📰 Checkpoint ./script.js:789197/1539:789214/1044 ./style.css:789214/58:789563/273 ./Tutorial.html:789563/703 ./index.html:789563/15

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
 let fps = 60;
+let width = 500;
+let height = 500;
+c.width = width;
+c.height = height;
 
 let objectList = {};
 
-//render loop
-let interval = setInterval(() => {
-  render();
-}, 1000 / fps);
+class Battler{
+  constructor(id, hp, width, height){
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.hp = hp;
+    this.currentFrame = 0;
+    this.totalFrame = sprite[id].totalFrame;
+    //stand, walk, attack
+    this.currentAction = "stand";
+    this.standAnimation = [];
+    this.walkAnimation = [];
+    this.attackAnimation = [];
+    //import
+    objectList[id] = this;
+  }
+  
+  jumpTo(x, y){
+    this.x = x;
+    this.y = y;
+  }
+}
 
 function render(){
   //empty
-  
+  ctx.clearRect(0, 0, width, height);
   //render
   for (let key in objectList){
     let object = objectList[key];
   }
-  ctx.drawImage()
+  ctx.drawImage(sprite[0].img, 0+128, 0+128, 128, 128, 0, 0, 128, 128)
+  console.log("aaa")
 }
 
 
@@ -35,34 +60,18 @@ let sprite = [
     totalFrame : 6 }//counting from 0
 ]
 
-sprite.forEach((v) => {
-  let src = v;
+sprite.forEach((v, i) => {
+  let img = new Image();
+  img.src = v.img;
+  sprite[i].img = img;
 });
+
 
 let char = new Battler(0, 1, 128, 128);
 char.x = 50;
 char.y = 50;
 
-class Battler{
-  constructor(id, hp, width, height){
-    this.id = id;
-    this.x = 0;
-    this.y = 0;
-    this.width = width;
-    this.height = height;
-    this.hp = hp;
-    this.currentFrame = 0;
-    //stand, walk, attack
-    this.currentAction = "stand";
-    this.standAnimation = [];
-    this.walkAnimation = [];
-    this.attackAnimation = [];
-    //import
-    objectList[id] = this;
-  }
-  
-  jumpTo(x, y){
-    this.x = x;
-    this.y = y;
-  }
-}
+//render loop
+let interval = setInterval(() => {
+  render();
+}, 1000 / fps);
