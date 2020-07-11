ea32e62f169559277307a7cbe1c2872c80f8a30a
Glitch (hello-webpage) <none>
2020-07-11 19:25:57 +0000

    🏏🍭 Checkpoint ./script.js:789214/307 ./Game.html:789563/630 ./index.html:789563/370

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
+let fps = 60;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class battler{
+  constructor(hp, width, height){
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.hp = hp;
+    this.walkAnimation = [];
+    this.attackAnimation = [];
+  }
+}
