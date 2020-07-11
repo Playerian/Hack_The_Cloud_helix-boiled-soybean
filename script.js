1574da26ef9bfd6e879ac70b9f4477402d4b1405
Glitch (hello-webpage) <none>
2020-07-11 19:37:06 +0000

    🏇🎠 Checkpoint ./script.js:789214/430 ./.glitch-assets:789197/396 ./index.html:789563/422 ./style.css:789214/16:789563/103

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
 let fps = 60;
 
+let objectList = {};
 
+//render loop
+let interval = setInterval(() => {
+  render();
+}, 1000 / fps);
 
-
-
-
+function render(){
+  
+}
 
 
 
@@ -28,12 +33,17 @@ let fps = 60;
 
 
 class battler{
-  constructor(hp, width, height){
+  constructor(id, hp, width, height){
+    this.id = id;
     this.x = 0;
     this.y = 0;
     this.width = width;
     this.height = height;
     this.hp = hp;
+    this.currentFrame = 0;
+    //stand, walk, attack
+    this.currentAction = "stand";
+    this.standAnimation = [];
     this.walkAnimation = [];
     this.attackAnimation = [];
   }
