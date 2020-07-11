72c1e31bd5945682a385e67bb99c9bc7c075362a
Glitch (hello-webpage) <none>
2020-07-11 23:29:46 +0000

    🐙💫 Checkpoint ./Tutorial.html:789563/827 ./script.js:789214/14644 ./style.css:789563/94 ./.glitch-assets:789197/367

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let objectList = {};
 let keyList = {};
 
 //constructors
-class Battler{
-  constructor(id, hp, width, height){
+class Battler {
+  constructor(id, hp, width, height) {
     this.id = id;
     this.x = 0;
     this.y = 0;
@@ -31,183 +31,232 @@ class Battler{
     //import
     objectList[id] = this;
   }
-    
-  jumpTo(x, y){
+
+  jumpTo(x, y) {
     this.x = x;
     this.y = y;
   }
-  
-  changeAction(action){ //animation
-    if (action !== "stand" && action !== "walk" && action !== "attack"){
+
+  changeAction(action) {
+    //animation
+    if (action !== "stand" && action !== "walk" && action !== "attack") {
       return;
     }
     this.currentFrame = 0;
     this.totalFrame = Math.floor(this[`${action}Animation`].width / this.width);
     this.currentAction = action;
   }
-  
-  isCollide(another){
+
+  isCollide(another) {
     //check x
-    if(this.facingRight === 1){
-      if(this.x <= another.x+128 && this.x+128 >= another.x){
+    if (this.facingRight === 1) {
+      if (this.x <= another.x + 128 && this.x + 128 >= another.x) {
         //check y
-        if(this.y <= another.y+128 && this.y+128 >= another.y){
-          console.log("collide")
+        if (this.y <= another.y + 128 && this.y + 128 >= another.y) {
+          console.log("collide");
         }
       }
-    }else{//facing left
-      if(this.x-256 <= another.x && this.x >= another.x){
-        if(this.y-128 <= another.y && this.y+128 >= another.y){
-          console.log("collide")
+    } else {
+      //facing left
+      if (this.x - 256 <= another.x && this.x >= another.x) {
+        if (this.y - 128 <= another.y && this.y + 128 >= another.y) {
+          console.log("collide");
         }
       }
     }
   }
-  
 }
-  
-class Projectile extends Battler{
-  constructor(id, hp, width, height){
-   
+
+class Projectile extends Battler {
+  constructor(id, hp, width, height) {
     //import
     objectList[id] = this;
   }
 }
 
-class Main extends Battler{
-  constructor(id, hp, width, height){
+class Main extends Battler {
+  constructor(id, hp, width, height) {
     super(id, hp, width, height);
     this.isMainChar = true;
   }
 }
 
-class Mobs extends Battler{
-  constructor(id, hp, width, height){
+class Mobs extends Battler {
+  constructor(id, hp, width, height) {
     super(id, hp, width, height);
     this.isMainChar = false;
-    this.AI = [];
+    this.AI = {
+      initial: ["toPlayer"],
+      repeat: ["attack"]
+    };
+    this.act;
   }
   
-  behavior(){ //should run every time handlemoveframe function runs
-    
+  onAttack(){
+    //fill whatever
+  }
+
+  behavior() {
+    //should run every time handlemoveframe function runs
+    //check if act exist
+    if (this.act) {
+      //act
+      //toPlayer
+      if (this.act === "toPlayer"){
+        //check if collide
+        if (this.collide)
+      }
+      
+      //resolve act
+      
+    } else {
+      //no act
+      //read AI
+      //do initial
+      if (this.AI.initial.length > 0) {
+        this.act = this.AI.initial[0];
+        this.AI.initial.shift();
+      } else {
+        //do repeat
+        this.act = this.AI.repeat[0];
+        this.AI.repeat.push(this.AI.repeat.shift());
+      }
+    }
   }
   
+  resolveAct(act){
+    if (this.act === act){
+      this.act = undefined;
+    }
+  }
 }
 
 //handling functions
-function render(){
+function render() {
   //empty
   ctx.clearRect(0, 0, width, height);
   //saving
   ctx.save();
   //render
-  for (let key in objectList){
+  for (let key in objectList) {
     let object = objectList[key];
     let objectX = object.x;
     let objectY = object.y;
     let oWidth = object.width;
     let oHeight = object.height;
     let currentFrame = object.currentFrame;
-    
-    if (object.facingRight === 0){
+
+    if (object.facingRight === 0) {
       ctx.translate(width, 0);
       ctx.scale(-1, 1);
-      ctx.drawImage(object[`${object.currentAction}Animation`], currentFrame * oWidth, 0, oWidth, oHeight, width - objectX, objectY, oWidth, oHeight);
-      
-      ctx.restore();
-      ctx.beginPath()
-      ctx.strokeStyle = "red"
-      ctx.rect(objectX-128,objectY,128,128)
-      ctx.stroke()
-    
-    }else{
-      ctx.drawImage(object[`${object.currentAction}Animation`], currentFrame * oWidth, 0, oWidth, oHeight, objectX, objectY, oWidth, oHeight); 
-    
-      //ctx.restore();
-      ctx.beginPath()
-      ctx.strokeStyle = "red"
-      ctx.rect(objectX,objectY,128,128)
-      ctx.stroke()
-    }
-     
-    //ctx.restore();
+      ctx.drawImage(
+        object[`${object.currentAction}Animation`],
+        currentFrame * oWidth,
+        0,
+        oWidth,
+        oHeight,
+        width - objectX,
+        objectY,
+        oWidth,
+        oHeight
+      );
 
+      ctx.restore();
+      ctx.beginPath();
+      ctx.strokeStyle = "red";
+      ctx.rect(objectX - 128, objectY, 128, 128);
+      ctx.stroke();
+    } else {
+      ctx.drawImage(
+        object[`${object.currentAction}Animation`],
+        currentFrame * oWidth,
+        0,
+        oWidth,
+        oHeight,
+        objectX,
+        objectY,
+        oWidth,
+        oHeight
+      );
+
+      //ctx.restore();
+      ctx.beginPath();
+      ctx.strokeStyle = "red";
+      ctx.rect(objectX, objectY, 128, 128);
+      ctx.stroke();
+    }
+
+    //ctx.restore();
   }
 }
 
-function handleKeys(){
+function handleKeys() {
   let checkMove = false;
-  if (keyList["w"] || keyList["ArrowUp"]){
+  if (keyList["w"] || keyList["ArrowUp"]) {
     mainChar.jumpTo(mainChar.x, mainChar.y - mainChar.speed);
     checkMove = true;
   }
-  if (keyList["a"] || keyList["ArrowLeft"]){
+  if (keyList["a"] || keyList["ArrowLeft"]) {
     mainChar.jumpTo(mainChar.x - mainChar.speed, mainChar.y);
-    if (mainChar.facingRight === 1){
+    if (mainChar.facingRight === 1) {
       mainChar.facingRight = 0;
       mainChar.x += mainChar.width;
     }
     checkMove = true;
   }
-  if (keyList["s"] || keyList["ArrowDown"]){
+  if (keyList["s"] || keyList["ArrowDown"]) {
     mainChar.jumpTo(mainChar.x, mainChar.y + mainChar.speed);
     checkMove = true;
   }
-  if (keyList["d"] || keyList["ArrowRight"]){
+  if (keyList["d"] || keyList["ArrowRight"]) {
     mainChar.jumpTo(mainChar.x + mainChar.speed, mainChar.y);
-    if (mainChar.facingRight === 0){
+    if (mainChar.facingRight === 0) {
       mainChar.facingRight = 1;
       mainChar.x -= mainChar.width;
     }
     checkMove = true;
   }
-  if (keyList["j"]){
-    if (mainChar.currentAction !== "attack"){
+  if (keyList["j"]) {
+    if (mainChar.currentAction !== "attack") {
       mainChar.changeAction("attack");
     }
   }
-  if (mainChar.currentAction !== "attack"){
-    if (checkMove){
-      if (mainChar.currentAction !== "walk"){
+  if (mainChar.currentAction !== "attack") {
+    if (checkMove) {
+      if (mainChar.currentAction !== "walk") {
         mainChar.changeAction("walk");
       }
-    }else{
+    } else {
       mainChar.changeAction("stand");
     }
   }
 }
 
-function handleMoveFrames(){
-  for (let key in objectList){
+function handleMoveFrames() {
+  for (let key in objectList) {
     let object = objectList[key];
     object.currentFrame += 1;
-    if (object.currentFrame >= object.totalFrame){
+    if (object.currentFrame >= object.totalFrame) {
       object.currentFrame = 0;
       //check if attacking
-      if (object.currentAction === "attack"){
+      if (object.currentAction === "attack") {
         object.changeAction("walk");
       }
     }
-    if (object.behavior){
+    if (object.behavior) {
       object.behavior();
     }
   }
 }
 
 //keyboard events
-$(document).keydown(function(e){
+$(document).keydown(function(e) {
   keyList[e.key] = true;
 });
 
-$(document).keyup(function(e){
+$(document).keyup(function(e) {
   keyList[e.key] = false;
 });
-
-
-
-
-
-
 
 let sprite = [
   // {
@@ -215,24 +264,32 @@ let sprite = [
   //   walk: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FcharAtk1Sprite.png?v=1594495768907",
   //   attack : "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FcharAtk1Sprite.png?v=1594495768907",
   // },
-  
-   {//guy
-    stand: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyWalk.png?v=1594501701844",
-    walk: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyWalk.png?v=1594501701844",
-    attack: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyAttack.png?v=1594502053544",
-    hitBox: [],//[[offset x, offset y, width, height],[]]
+
+  {
+    //guy
+    stand:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyWalk.png?v=1594501701844",
+    walk:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyWalk.png?v=1594501701844",
+    attack:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FguyAttack.png?v=1594502053544",
+    hitBox: [] //[[offset x, offset y, width, height],[]]
   },
-  
-  {//turrent
-    stand: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentWalk.png?v=1594499006955",
-    walk: "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentWalk.png?v=1594499006955",
-    attack : "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentShoot.png?v=1594499885384",
-  }, 
-]
+
+  {
+    //turrent
+    stand:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentWalk.png?v=1594499006955",
+    walk:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentWalk.png?v=1594499006955",
+    attack:
+      "https://cdn.glitch.com/2d713a23-b2e0-4a6b-9d5c-61c597ba6d8e%2FturrentShoot.png?v=1594499885384"
+  }
+];
 
 sprite.forEach((v, i) => {
-  for (let keys in v){
-    if (keys === "stand" || keys === "walk" || keys === "attack"){
+  for (let keys in v) {
+    if (keys === "stand" || keys === "walk" || keys === "attack") {
       let href = v[keys];
       let img = new Image();
       img.src = href;
@@ -241,17 +298,16 @@ sprite.forEach((v, i) => {
   }
 });
 
-
 let mainChar = new Main(0, 1, 128, 128);
 mainChar.jumpTo(50, 50);
 
-let mob = new Mobs(1,10,128,128)
-mob.jumpTo(120,120)
+let mob = new Mobs(1, 10, 128, 128);
+mob.jumpTo(120, 120);
 
 //render loop
 let interval = setInterval(() => {
   handleKeys();
   handleMoveFrames();
-  mainChar.isCollide(mob)
+  mainChar.isCollide(mob);
   render();
 }, 1000 / fps);
